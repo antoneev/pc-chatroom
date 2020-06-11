2777db714c12d5581cd027ae82dfb32bd7772103
antoneev <47998793+antoneev@users.noreply.github.com>
2020-06-10 20:06:02 -0500

    Add files via upload

---
+++ SocketClient/Form2.vb
@@ -0,0 +1,126 @@
+Imports System.Text
+Imports System.Net.Sockets
+
+
+Public Class Form2
+    Inherits System.Windows.Forms.Form
+
+    Public Delegate Sub DisplayInvoker(ByVal t As String)
+
+#Region " Windows Form Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        'Add any initialization after the InitializeComponent() call
+
+    End Sub
+
+    'Form overrides dispose to clean up the component list.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+    Friend WithEvents txtSend As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.Container
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Me.txtSend = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtSend
+        '
+        Me.txtSend.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSend.Location = New System.Drawing.Point(12, 131)
+        Me.txtSend.Name = "txtSend"
+        Me.txtSend.Size = New System.Drawing.Size(390, 20)
+        Me.txtSend.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(327, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(327, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Enter Port:"
+        '
+        'Form2
+        '
+        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+        Me.ClientSize = New System.Drawing.Size(424, 163)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtSend)
+        Me.Name = "Form2"
+        Me.Text = "Port Setting"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Quit button closes the box
+        Me.Dispose()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    'Pubically creates the Port variable 
+    Public Port As Integer
+    'Eventhandler for the OK button
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Passes in the port from the textbox
+        Port = Integer.Parse(txtSend.Text)
+        'Moves to IP form when OK pressed
+        Dim OBJ As New Form3
+        'Shows the port
+        OBJ.Show()
+        'Hides this form
+        Me.Hide()
+    End Sub
+
+#End Region
+
+
+End Class
